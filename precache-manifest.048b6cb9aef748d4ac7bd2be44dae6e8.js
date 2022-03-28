0bbba7235b77ec99639d7f98ddae8cc9c865a5eb
ankit8609 <87761962+ankit8609@users.noreply.github.com>
2022-03-28 16:09:43 +0530

    Updates

---
+++ precache-manifest.048b6cb9aef748d4ac7bd2be44dae6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b77d39633f462e69e3c",
+    "url": "/examplan/static/css/main.9d9f88c0.chunk.css"
+  },
+  {
+    "revision": "1b77d39633f462e69e3c",
+    "url": "/examplan/static/js/main.1b77d396.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/examplan/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "7623fa626bfd43536598",
+    "url": "/examplan/static/js/2.7623fa62.chunk.js"
+  },
+  {
+    "revision": "37bc23aa4d8613067766",
+    "url": "/examplan/static/js/runtime~main.37bc23aa.js"
+  },
+  {
+    "revision": "0e7fb6214ed17847a393f87e62fcdb17",
+    "url": "/examplan/index.html"
+  }
+];
